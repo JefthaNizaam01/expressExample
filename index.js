ce52e50ae857a5e629851944ee34322853e254fd
Name <nizaam01.jeftha@gmail.com>
2024-08-01 15:26:42 +0200

    products

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ config()
 
 const app = express()
 const port = +process.env.PORT || 4000
+const productUrl = 'http://localhost:3000/results'
 app.use(express.static('/static'))
+// Retrieve data
+async function getData () {
+    return await (await fetch(productURL)).json()
+}
 app.get('/',(req, res) => {
     res.status(200). sendFile(path.resolve('./static/index.html'))
 })
@@ -13,6 +18,7 @@ app.get('/',(req, res) => {
 app.get('/about',(req, res) => {
     res.status(200). sendFile(path.resolve('./static/about.html'))
 })
+app.get('/products',(req,res) =>)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
